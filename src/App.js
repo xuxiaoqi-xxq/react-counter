9353abccd1eda71d8bacca2bc03682e611044872
xuxiaoqi-xxq <xxq_work@163.com>
2020-08-03 16:40:20 +0800

    [eva] feature: add input of counter number

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <CounterGroup />
     </div>
-  
+
   );
 }
 
--- src/components/CounterGroup/index.jsx
+++ src/components/CounterGroup/index.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import Counter from '../Counter/index'
+import InputNumber from '../InputNumber/index'
 
 class CounterGroup extends React.Component{
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            counterNumber: 1
+        };
+    }
+
+    getInputNumber(inputCounterNumber) {
+        console.log(inputCounterNumber);
+        this.setState({counterNumber: inputCounterNumber});
+    }
+
     render() {
         return (
-            new Array(9).fill(0).map((value, index) => <Counter key = {index}/>)
+            [
+                <InputNumber getInputNumber={this.getInputNumber.bind(this)}/>,
+                <div>{new Array(this.state.counterNumber).fill(parseInt(this.state.counterNumber)).map((value, index) => <Counter key = {index}/>)}</div>
+            ]
         );
     }
 }
